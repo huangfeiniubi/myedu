3c554eeac8a7695bba5cab5c4f534b7660de4f54
unknown <15021476126@163.com>
2019-03-21 16:14:12 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,78 @@
+# 声明一个 int_demo 方法
+def int_demo(aint1):
+    print(aint1)
+    print(type(aint1))
+    # 声明 aint 变量   并赋值 1
+    # aint = 6
+    # # 打印 aint 的值
+    # print(aint)
+    # # 打印 aint 类型 type(aint):   获取aint的类型
+
+
+def float_demo():
+    afloat = 1.9
+    print(afloat)
+    print(type(afloat))
+    return afloat +1
+
+def add(aint,bint):
+    return aint - bint
+
+if __name__ == '__main__':
+    s = add(7,4)
+    print(s)
+
+
+    # int_demo(0.2)
+
+
+
+
+    # 声明一个 int_demo 方法
+def str_demo():
+        # 声明 astr 变量  并赋值 1
+        astr ='1'
+
+        #打印 astr
+
+        print(astr)
+        # 打印astr的类型  type（astr）: 获取astr的类型
+        print(type(astr))
+
+
+# 声明add方法 参数有两个aint，bint
+def add(aint, bint):
+   # 打印aint值
+    print(aint)
+    #打印bint值
+    print(bint)
+
+    return aint + bint
+#
+#
+# if __name__ == '__main__':
+#     result = add(1, 2)
+#     print(result)
+
+
+
+
+
+
+
+
+
+def abc(aint1,aint2):
+    # print(aint1)
+    # print(aint2)
+    return aint1 + aint2
+
+
+
+
+
+
+
+
+
+
